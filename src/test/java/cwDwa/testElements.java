f3062c383374be5fe6c4d6c9577c58310f346ee2
warhir <krzyzopolski.mariusz@gmail.com>
2023-01-25 22:30:16 +0100

    zad2

--- src/test/java/cwDwa/testElements.java
+++ src/test/java/cwDwa/testElements.java
@@ -6,15 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class testElements {
     private static WebDriver driver;
@@ -26,5 +20,55 @@ public class testElements {
         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/resources/geckodriver");
         driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void testFirstAndThirdLink() {
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.xpath("//article[@id='r1-0']/div[2]/h2/a/span")).click();
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.xpath("//article[@id='r1-2']/div[2]/h2/a/span")).click();
+        assertEquals("The Witcher 3: Wild Hunt - Official Website ", driver.getTitle());
+    }
+
+    @Test
+    public void testJsGet(){
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        WebElement firstLink = driver.findElement(By.xpath("//article[@id='r1-0']/div[2]/h2/a/span"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", firstLink);
+        assertEquals("Wiedźmin | Oficjalna witryna Netflix", driver.getTitle());
+    }
+
+    @Test
+    public void testByClassName() {
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.className("LQNqh2U1kzYxREs65IJu")).click();
+        assertEquals("Wiedźmin | Oficjalna witryna Netflix", driver.getTitle());
+    }
+
+    @Test
+    public void testByID() {
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.id("r1-0")).click();
+        assertEquals("Wiedźmin | Oficjalna witryna Netflix", driver.getTitle());
+    }
+
+    @Test
+    public void testByCSS() {
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.cssSelector(".eVNpHGjtxRBq_gLOfGDr")).click();
+        assertEquals("Wiedźmin | Oficjalna witryna Netflix", driver.getTitle());
+    }
+
+    @Test
+    public void testByTag() {
+        driver.get("https://duckduckgo.com/?q=witcher&atb=v345-1&ia=web");
+        driver.findElement(By.tagName("h2")).click();
+        assertEquals("Wiedźmin | Oficjalna witryna Netflix", driver.getTitle());
     }
 }
